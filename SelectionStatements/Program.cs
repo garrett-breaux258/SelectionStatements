03af56b2d35b8c39010d3de86496478d89aa9b86
Garrett Breaux <garrett.breaux1@gmail.com>
2021-06-23 12:08:37 -0500

    initial commit

---
+++ SelectionStatements/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SelectionStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Can you guess my favorite number?");
+
+            var r = new Random();
+            var favoriteNumber = r.Next(1, 1000);
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favoriteNumber)
+            {
+                Console.WriteLine("Too Low");
+
+            }
+            else if (userInput > favoriteNumber)
+            {
+                Console.WriteLine("Too High");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind");
+
+            }
+        }
+
+    }
+}
